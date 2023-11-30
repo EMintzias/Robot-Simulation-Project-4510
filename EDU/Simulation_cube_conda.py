1c5485ad049b585bb53fc026cb4ff70e81801a21
EMintzias <eam2285@columbia.edu>
2023-11-30 00:10:44 -0500

    runnin dtype

--- EDU/Simulation_cube_conda.py
+++ EDU/Simulation_cube_conda.py
@@ -163,113 +163,14 @@ class Simulate:
     
     ########################### RUN METHODS #######################################
     # TODO run faster!
-    def update_mass(self, mass):
-        # Initial force is 0
-        # GRAVITATIONAL FORCE
-        # Update F
-        F = mass.m * self.G
-        # SPRING FORCE
-        # Loop over all springs
-        for spring in mass.springs:
-            # If spring connected to that mass
-            if spring.m1 == mass or spring.m2 == mass:
-                # Calculate spring force and actuator length based on time
-                L = np.linalg.norm(spring.m1.p - spring.m2.p)
-                Lo = spring.L0*(1+spring.b * np.sin(self.omega*self.T + spring.c))
-                # Update F_spring in the vector direction from m2 to m1 (unit vect (p1-p1)/dist)
-                F_spring = spring.k * (L - Lo) * (spring.m1.p - spring.m2.p) / L
-                
-                # Update F with appropriate sign
-                if spring.m1 == mass:
-                    F -= F_spring
-                else:
-                    F += F_spring
-        # FRICTION FORCE
-        if mass.p[2] <= 0:
-            Fp = [F[0], F[1], 0]
-            Fn = [0, 0, F[2]]
-            if np.linalg.norm(Fp) < np.linalg.norm(Fn)*self.mu_s:
-                F -= Fp
-            else:
-                F += Fp/np.linalg.norm(Fp)*(-1)*np.linalg.norm(Fn)*self.mu_k
-        # GROUND COLLISION FORCE
-        # Ground collision check and response
-        if mass.p[2] < 0:
-            F += np.array([0, 0, -self.Kc * mass.p[2]])
-        
-        # UPDATE ACCELERATION
-        mass.a = F / mass.m
-        # UPDATE VELOCITY
-        mass.v += mass.a * self.dt
-        mass.v *= self.b # dampening
-        # UPDATE POSITION
-        mass.p += mass.v * self.dt
-        return mass
-    
-    def Body_Advance_step(self):
-        #Update masses
-        self.body.masses = [self.update_mass(mass) for mass in self.body.masses]
-        
-        #update simulation
-        self.T+=self.dt
-        self.global_step +=1
-        pass
-    
-    
-    
-    def update_mass_git(self, mass_ind,mass):
-        # Initial force is 0
-        # GRAVITATIONAL FORCE
-        # Update F
-        F = mass.m * self.G
-        # SPRING FORCE
-        # Loop over all springs
-        for spring in mass.springs:
-            # If spring connected to that mass
-            if spring.m1 == mass or spring.m2 == mass:
-                # Calculate spring force and actuator length based on time
-                L = np.linalg.norm(spring.m1.p - spring.m2.p)
-                Lo = spring.L0*(1+spring.b * np.sin(self.omega*self.T + spring.c))
-                # Update F_spring in the vector direction from m2 to m1 (unit vect (p1-p1)/dist)
-                F_spring = spring.k * (L - Lo) * (spring.m1.p - spring.m2.p) / L
-                
-                # Update F with appropriate sign
-                if spring.m1 == mass:
-                    F -= F_spring
-                else:
-                    F += F_spring
-        # FRICTION FORCE
-        if mass.p[2] <= 0:
-            Fp = [F[0], F[1], 0]
-            Fn = [0, 0, F[2]]
-            if np.linalg.norm(Fp) < np.linalg.norm(Fn)*self.mu_s:
-                F -= Fp
-            else:
-                F += Fp/np.linalg.norm(Fp)*(-1)*np.linalg.norm(Fn)*self.mu_k
-        # GROUND COLLISION FORCE
-        # Ground collision check and response
-        if mass.p[2] < 0:
-            F += np.array([0, 0, -self.Kc * mass.p[2]])
-        
-        # UPDATE ACCELERATION
-        mass.a = F / mass.m
-        # UPDATE VELOCITY
-        mass.v += mass.a * self.dt
-        mass.v *= self.b # dampening
-        # UPDATE POSITION
-        mass.p += mass.v * self.dt
-        return mass
-    
-    
+   
+ 
     def Body_Advance_step_git(self,old_masses_arr,springs_arr):
-        #Update masses
-        #self.body.masses = [self.update_mass_git(mass) for mass in self.body.masses]
+        #If we need to do 
         
         #update simulation
         new_masses = old_masses_arr.copy()
         for mass_ind,mass in enumerate(new_masses): 
-            #masses_arr[mass_ind] = self.update_mass_git(mass_ind,mass)
-            #TODO make copy and work off of copy for updates
             
             # Initial force is 0
             # GRAVITATIONAL FORCE
@@ -320,7 +221,7 @@ class Simulate:
             new_masses[mass_ind] = mass #i think this is redundant since it happened above?
             
             
-        
+        #if we need to delete all obj references we can make another step function to call the above and these lines
         self.T+=self.dt
         self.global_step +=1
         #return new_masses_arr, new_springs_arr 
@@ -390,6 +291,7 @@ class Simulate:
                 #self.Body_Advance_step()
                 masses_arr, _ = self.Body_Advance_step_git(masses_arr, springs_arr)
                 self.update_mass_obj(masses_arr)
+                
                 #print(f"Running {len(self.body.springs)/(time.time() - start_time):.6f} springs/sec")
 
                 # _________PLOTING_________
